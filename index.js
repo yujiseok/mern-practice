f21d95c87d658cffdbe6f3f240c71200b1933770
yujiseok <koesijuy@urbanisom.com>
2022-05-19 17:54:47 +0900

    로그아웃

--- index.js
+++ index.js
@@ -84,6 +84,26 @@ app.get("/api/users/auth", auth, (req, res) => {
   });
 });
 
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate(
+    {
+      _id: req.user._id,
+    },
+    { token: "" },
+    (err, user) => {
+      if (err) {
+        return res.json({
+          success: false,
+          err,
+        });
+      }
+      return res.status(200).send({
+        success: true,
+      });
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`서버 돌아가는 중 포트는 ${port}`);
 });
