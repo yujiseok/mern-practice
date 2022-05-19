d15b4221e3ba4773a4cbbbd00201731f1f8ca9d4
yujiseok <koesijuy@urbanisom.com>
2022-05-19 16:20:24 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://jiseok:1234@cluster0.qqrna.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(config.mongoURI)
   .then(() => console.log("db connected"))
   .catch((err) => console.err(err));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입시 필요한 정보를 클라이언트에서 가져온 후 데이터베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => {
