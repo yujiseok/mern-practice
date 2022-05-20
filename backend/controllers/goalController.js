0c4302f2b76234db840e885aaf262e16572a900c
yujiseok <koesijuy@urbanisom.com>
2022-05-20 16:09:25 +0900

    backend

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,15 +1,14 @@
 const asyncHandler = require('express-async-handler');
 
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 // @desc  Get goals
 // @route Get /api/goals
 // @access Pivate
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
-  res.status(200).json({
-    goals,
-  });
+  const goals = await Goal.find({user: req.user.id});
+  res.status(200).json(goals);
 });
 
 // @desc  Set goals
@@ -22,11 +21,10 @@ const setGoals = asyncHandler(async (req, res) => {
   }
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
 
-  res.status(200).json({
-    goal,
-  });
+  res.status(200).json(goal);
 });
 
 // @desc  Update goals
@@ -38,6 +36,20 @@ const updateGoals = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error('Goal not found');
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // check user
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  // make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -57,7 +69,19 @@ const deleteGoals = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Goal not found');
   }
+  const user = await User.findById(req.user.id);
 
+  // check user
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  // make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
   await goal.remove();
   res.status(200).json({id: req.params.id});
 });
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const colors = require("colors");
-const doteenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddleware");
-const connectDB = require("./config/db");
+const express = require('express');
+const colors = require('colors');
+const doteenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -10,9 +10,10 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
-app.use("/api/goals", require("./routes/goalRoutes"));
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
